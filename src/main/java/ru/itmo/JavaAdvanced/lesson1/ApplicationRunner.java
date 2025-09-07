1baf88161979c8912d774735d7808af2efd5a9ea
gorlenko_ira <gorlenkoircho96@mail.ru>
2025-09-07 16:16:34 +0300

    fix: replace const to local variable

--- src/main/java/ru/itmo/JavaAdvanced/lesson1/ApplicationRunner.java
+++ src/main/java/ru/itmo/JavaAdvanced/lesson1/ApplicationRunner.java
@@ -6,11 +6,11 @@ import ru.itmo.JavaAdvanced.lesson1.dto.ExampleDto;
 
 @SpringBootApplication
 public class ApplicationRunner {
-    public static final ExampleDto EXAMPLE_DTO = new ExampleDto();
 
 	public static void main(String[] args) {
-		EXAMPLE_DTO.setId(1L);
-        System.out.println(EXAMPLE_DTO.getId());
+		ExampleDto exampleDto = new ExampleDto();
+        exampleDto.setId(1L);
+        System.out.println(exampleDto.getId());
 
         SpringApplication.run(ApplicationRunner.class, args);
 	}
